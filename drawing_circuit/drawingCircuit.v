0f972835ccffdf62dede0b2d3fc182073844cc45
Zachary Mar <53539853+ZacharyMar@users.noreply.github.com>
2023-12-03 15:19:19 -0500

    Accommodate new screen size + colours

--- drawing_circuit/drawingCircuit.v
+++ drawing_circuit/drawingCircuit.v
@@ -1,6 +1,6 @@
 `timescale 1 ns / 1 ns
 
-module drawingCircuit #(parameter SCREEN_WIDTH = 160,parameter SCREEN_HEIGHT = 120)(
+module drawingCircuit #(parameter SCREEN_WIDTH = 320,parameter SCREEN_HEIGHT = 240)(
     iClk,
     iResetn,
     iBtnL,
@@ -16,27 +16,30 @@ module drawingCircuit #(parameter SCREEN_WIDTH = 160,parameter SCREEN_HEIGHT = 1
     oY_pixel,
     oStartTransmission,
     oEnableMouse,
-    oPlot
+    oPlot,
+    oChipSelect
 	);
     parameter CELL_DIMENSION = 5;
 	 parameter UPPER_BITS = $clog2((SCREEN_WIDTH / CELL_DIMENSION) > (SCREEN_HEIGHT / CELL_DIMENSION)? (SCREEN_WIDTH / CELL_DIMENSION):(SCREEN_HEIGHT / CELL_DIMENSION));
     //block inputs
     input wire iClk, iResetn, iBtnL, iBtnR, iClear, iSlot0, iSlot1;
     input wire [UPPER_BITS-1:0] iX_cell, iY_cell; //xy from mouse (represents position on smaller drawing grid (32 x 24 on 160 x 120 display))
-    input wire [2:0] iColour; //colour data from switch input
+    input wire [8:0] iColour; //colour data from switch input
     //block outputs
-    output wire [2:0] oColour; //also known as data to ram, used for VGA and data into ram controller
+    output wire [8:0] oColour; //also known as data to ram, used for VGA and data into ram controller
     output wire [$clog2(SCREEN_WIDTH):0] oX_pixel; //xy to VGA
 	 output wire [$clog2(SCREEN_HEIGHT):0] oY_pixel;
     output wire oPlot;
     output wire oStartTransmission;
     output wire oEnableMouse;
+    output wire oChipSelect;
 
-	
+    
     //inner wires to memory controller module
-    wire ChipSelect, wren, q;
-    wire [14:0] address;
-
+    wire ChipSelect, wren;
+	 wire [8:0] q;
+    wire [16:0] address;
+    assign oChipSelect = ChipSelect;
 	integratedCircuit #(.SCREEN_WIDTH(SCREEN_WIDTH), .SCREEN_HEIGHT(SCREEN_HEIGHT)) INTEGRATED_CIRCUIT(
 	 .iClk(iClk),
     .iResetn(iResetn),
@@ -56,10 +59,12 @@ module drawingCircuit #(parameter SCREEN_WIDTH = 160,parameter SCREEN_HEIGHT = 1
     .oY_pixel(oY_pixel),
     .oChipSelect(ChipSelect),
     .oStartTransmission(oStartTransmission),
-    .oEnableMouse(oEnableMouse)
+    .oEnableMouse(oEnableMouse),
+	 .oPlot(oPlot)
 	);
 
 	memory_controller MEMORY_CONTROLLER(
+	 .iResetn(iResetn),
 	 .iClk(iClk), 
     .iData(oColour), 
     .iAddress(address), 
