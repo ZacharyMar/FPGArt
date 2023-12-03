d45c7a1d3d19055c37136ddaffe2ad0e86a35c23
Zachary Mar <53539853+ZacharyMar@users.noreply.github.com>
2023-12-03 15:14:38 -0500

    Added functionality for resetting saves

--- drawing_circuit/ramDatapath.v
+++ drawing_circuit/ramDatapath.v
@@ -1,8 +1,8 @@
-`timescale 1 ns / 1ns
+`timescale 1 ns / 1 ns
 module ramDatapath
     #(
-	parameter SCREEN_WIDTH = 160,
-	parameter SCREEN_HEIGHT = 120
+	parameter SCREEN_WIDTH = 320,
+	parameter SCREEN_HEIGHT = 240
 	)
     (
     iClk,
@@ -18,18 +18,18 @@ module ramDatapath
     oChipSelect
 );
 input wire iClk, iReset;
-input wire [3:0] iState;
+input wire [4:0] iState;
 
 //output to fsm
 output reg oDone, oChipSelect;
 
 //data read signals for first buffer
-input wire [2:0] iQ_ram;
-output reg [14:0] oAddress_ram;
+input wire [8:0] iQ_ram;
+output reg [16:0] oAddress_ram;
 //removed wren as the controlpath and memory controller's chipselect and datapath select signals should handle when we are reading from memory
 
 //output to VGA
-output reg [2:0] oColour;
+output reg [8:0] oColour;
 output reg oPlot;
 output reg [$clog2(SCREEN_WIDTH > SCREEN_HEIGHT ? SCREEN_WIDTH:SCREEN_HEIGHT):0] ox, oy;
 
@@ -38,12 +38,17 @@ reg [$clog2(SCREEN_WIDTH > SCREEN_HEIGHT ? SCREEN_WIDTH:SCREEN_HEIGHT):0] x_coun
 reg load_mem, start_load;
 
 //relevant memory states
-localparam  IDLE = 4'd0,
-            CHANGE_STATE0 = 4'd9,
-            CHANGE_STATE0_LOAD = 4'd10,
-            CHANGE_STATE1 = 4'd11,
-            CHANGE_STATE1_LOAD = 4'd12;
-always@ (posedge iClk) begin
+localparam  IDLE = 5'd0,
+            CHANGE_STATE0 = 5'd9,
+            CHANGE_STATE0_LOAD = 5'd10,
+            CHANGE_STATE1 = 5'd11,
+            CHANGE_STATE1_LOAD = 5'd12,
+				CLEAR_RESET_WAIT0 = 5'd13,
+				CLEAR_RESET_SLOT0 = 5'd14,
+				CLEAR_RESET_WAIT1 = 5'd15,
+				CLEAR_RESET_SLOT1 = 5'd16;
+				
+always@ (posedge iClk, negedge iReset) begin
     if(!iReset) begin
         x_count <= 0;
         y_count <= 0;
@@ -52,7 +57,7 @@ always@ (posedge iClk) begin
         oDone <= 0;
         ox <= 0;
         oy <= 0;
-        oColour <= 3'b111;
+        oColour <= 9'b111111111;
         oPlot <= 0;
         start_load <= 0;
         oChipSelect <= 0;
@@ -69,11 +74,11 @@ always@ (posedge iClk) begin
             end
             else if(load_mem)begin //to load a pixel from memory, pass address into BRAM module 1
                 load_mem <= 0;
-                oAddress_ram <= ({1'b0, y_count, 7'd0} + {1'b0, y_count, 5'd0} + {1'b0, x_count});
+                oAddress_ram <= ({1'b0, y_count, 8'd0} + {1'b0, y_count, 6'd0} + {1'b0, x_count});
             end
             else if(!load_mem)begin //to draw, enable plot and load the x, y, and c registers
 					  oColour <= iQ_ram;
-					  ox <= x_count;
+					  ox <= x_count-1;
 					  oy <= y_count;
 					  oPlot <= 1;
                 if(y_count == SCREEN_HEIGHT-1 && x_count == SCREEN_WIDTH -1)begin //if we are at the bottom right pixel then we are Done drawing the frame
@@ -107,11 +112,11 @@ always@ (posedge iClk) begin
             end
             else if(load_mem)begin //to load a pixel from memory, pass address into BRAM module 0
                 load_mem <= 0;
-                oAddress_ram <= ({1'b0, y_count, 7'd0} + {1'b0, y_count, 5'd0} + {1'b0, x_count});
+                oAddress_ram <= ({1'b0, y_count, 8'd0} + {1'b0, y_count, 6'd0} + {1'b0, x_count});
             end
             else if(!load_mem)begin //to draw, enable plot and load the x, y, and c registers
                 oColour <= iQ_ram;
-                ox <= x_count;
+                ox <= x_count-1;
                 oy <= y_count;  
 					 oPlot  <= 1;
                 if(y_count == SCREEN_HEIGHT-1 && x_count == SCREEN_WIDTH -1)begin 
@@ -136,6 +141,10 @@ always@ (posedge iClk) begin
             oChipSelect <= 0;
             oDone <= 0;
         end
+		  
+		  else if (iState == CLEAR_RESET_WAIT0) oChipSelect <= 0;
+		  
+		  else if (iState == CLEAR_RESET_WAIT1) oChipSelect <= 1;
 		  
 		  // Default case should set plot to 0
 		  else
